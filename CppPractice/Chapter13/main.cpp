ecd40e9701a5e0f2f8fe543144dc96e4a4124d2c
WolfhoundAD <Loners14@yandex.ru>
2023-07-21 14:21:47 +0300

    Fix remarks Chapter 13 and add empty 14-17 chapters

--- CppPractice/Chapter13/main.cpp
+++ CppPractice/Chapter13/main.cpp
@@ -13,17 +13,21 @@ int main()
   //Data on the price and circulation of each of the 15 magazines are known. Find the average
   //cost of magazines with a circulation of less than 10,000 copies.
   cout << "Chapter 13. Number 13.26." << endl;
-  const int n = 15;
-  magazine mag[n];
+  const int magazine_amount = 15;
+  const int quantity = 10000;
+  magazine mag[magazine_amount];
   int sum_price = 0, count = 0;
+  const pair<int, int> price_range = {10, 50};
+  const pair<int, int> circulation_range = {1000, 20000};
+  srand( time( NULL ) );
 
-  for ( int i = 0; i < n; i++ )
+  for ( int i = 0; i < magazine_amount; i++ )
   {
-    mag[i].price = rand() % 50 + 10;
-    mag[i].circulation = rand() % 20000 + 1000;
+    mag[i].price = rand() % ( price_range.second - price_range.first + 1 ) + price_range.first;
+    mag[i].circulation = rand() % ( circulation_range.second - circulation_range.first + 1 ) + circulation_range.first;
     cout << "Price " << i + 1 << ": " << mag[i].price << "  Circulation: " << mag[i].circulation << "\n";
 
-    if ( mag[i].circulation < 10000 )
+    if ( mag[i].circulation < quantity )
     {
       sum_price += mag[i].price;
       count++;
@@ -31,7 +35,7 @@ int main()
   }
 
   double average = sum_price / count;
-  cout << "The number of magazines with a circulation of less than 10,000: " << count;
+  cout << "The number of magazines with a circulation of less than " << quantity << ": " << count;
   cout << "\n Average price: " << average << "\n";
 
   return 0;
